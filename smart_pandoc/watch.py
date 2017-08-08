fb3c8a4a8898180b74bbfd8723fb109d07c44ebf
swo <swo@alum.mit.edu>
2017-08-08 16:18:11 -0400

    watch with callback

--- smart_pandoc/watch.py
+++ smart_pandoc/watch.py
@@ -1,19 +1,20 @@
-import watchdog.events, os.path, subprocess, sys
+import watchdog.events, os.path, sys
 
 class SpandocHandler(watchdog.events.FileSystemEventHandler):
-    def __init__(self, fn, command):
+    def __init__(self, fn, callback):
         '''
         fn : str
           file the to be watched (probably the output file)
-        command : list of strings
-          pandoc command to be run with subprocess
+        callback : function (no arguments)
+          function that will cause the update
         '''
         self.fn = os.path.abspath(fn)
-        self.command = command
+        self.callback = callback
 
     def on_any_event(self, event):
         if os.path.abspath(event.src_path) == self.fn:
             if event.event_type == 'modified':
+                self.callback.__call__()
                 subprocess.run(self.command)
             elif event.event_type == 'deleted':
                 print('watched file was deleted; quitting')
